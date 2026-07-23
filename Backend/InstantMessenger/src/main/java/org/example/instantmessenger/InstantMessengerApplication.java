96a33fb539da776282cd618d6f9628f71d622634
michalbuszka <318049@stud.umk.pl>
2026-07-23 17:40:00 +0200

    init

---
+++ Backend/InstantMessenger/src/main/java/org/example/instantmessenger/InstantMessengerApplication.java
@@ -0,0 +1,13 @@
+package org.example.instantmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstantMessengerApplication.class, args);
+    }
+
+}
---
+++ Backend/InstantMessenger/src/test/java/org/example/instantmessenger/InstantMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.instantmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
